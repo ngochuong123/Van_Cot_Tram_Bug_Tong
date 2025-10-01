c932b7e4d379d5066935b2163279cda585a4def1
Hoang Nguyen <hoanghuy4d@gmail.com>
2025-10-01 20:52:26 +0700

    feat(model): add StrongBrick class (attribute and method codebase

--- src/main/java/vn/uet/oop/arkanoid/model/bricks/StrongBrick.java
+++ src/main/java/vn/uet/oop/arkanoid/model/bricks/StrongBrick.java
@@ -1,4 +1,36 @@
 package vn.uet.oop.arkanoid.model.bricks;
 
-public class StrongBrick {
+import javafx.scene.canvas.GraphicsContext;
+
+public class StrongBrick extends Brick {
+    private int durabilityPoints = 2;
+
+    public StrongBrick(double x, double y, double width, double height) {
+        super(x, y, width, height);
+    }
+
+    @Override
+    public int takeHit() {
+        if (durabilityPoints > 0) {
+            durabilityPoints--;
+        }
+        return durabilityPoints;
+    }
+
+    @Override
+    public boolean isBroken() {
+        return durabilityPoints == 0;
+    }
+
+    @Override
+    public void update(double deltaTime) {
+        //update
+    }
+
+    @Override
+    public void render(GraphicsContext gc) {
+        // render
+    }
+
+
 }
