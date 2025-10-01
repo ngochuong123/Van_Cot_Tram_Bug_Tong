f10aa1a64a9ebfea1352a9200435c42a5ffea349
Hoang Nguyen <hoanghuy4d@gmail.com>
2025-10-01 20:25:30 +0700

    feat(model/brick): add Brick abstract class

--- src/main/java/vn/uet/oop/arkanoid/model/bricks/Brick.java
+++ src/main/java/vn/uet/oop/arkanoid/model/bricks/Brick.java
@@ -1,4 +1,11 @@
 package vn.uet.oop.arkanoid.model.bricks;
 
-public class Brick {
+import vn.uet.oop.arkanoid.model.GameObject;
+
+public abstract class Brick extends GameObject {
+    private int durabilityPoints;           // the number of hit to break brick
+
+    public abstract int takeHit();          // return the remaining durability points after taking hit
+    public abstract boolean isBroken();     // return true if the brick is broken
+
 }
