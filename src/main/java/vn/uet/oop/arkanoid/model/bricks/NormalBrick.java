e43bc1967ecde8f69499dddc8b9c3068eb56d640
Hoang Nguyen <hoanghuy4d@gmail.com>
2025-10-01 20:51:36 +0700

    feat(model): add NormalBrick class (attribute and method codebase)

--- src/main/java/vn/uet/oop/arkanoid/model/bricks/NormalBrick.java
+++ src/main/java/vn/uet/oop/arkanoid/model/bricks/NormalBrick.java
@@ -1,4 +1,37 @@
 package vn.uet.oop.arkanoid.model.bricks;
 
-public class NormalBrick {
+import javafx.scene.canvas.GraphicsContext;
+import vn.uet.oop.arkanoid.model.GameObject;
+
+public class NormalBrick extends Brick {
+    private int durabilityPoints = 1;
+
+    public NormalBrick(double x, double y, double width, double height) {
+        super(x, y, width, height);
+    }
+
+    @Override
+    public int takeHit() {
+        if (durabilityPoints > 0) {
+            durabilityPoints--;
+        }
+        return durabilityPoints;
+    }
+    @Override
+    public boolean isBroken() {
+        return durabilityPoints == 0;
+    }
+
+    @Override
+    public void update(double deltaTime) {
+        // not need update
+    }
+
+    @Override
+    public void render(GraphicsContext gc) {
+        //render
+
+    }
+
+
 }
