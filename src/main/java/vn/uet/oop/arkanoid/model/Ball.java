deea8a2fa5dad26e880d50a1cf3e3fbeafc3f951
Nguyenquanghong <24021495@vnu.edu.vn>
2025-10-08 15:23:35 +0700

    update MovableObject + Ball + Paddle

--- src/main/java/vn/uet/oop/arkanoid/model/Ball.java
+++ src/main/java/vn/uet/oop/arkanoid/model/Ball.java
@@ -1,27 +1,21 @@
+
 package vn.uet.oop.arkanoid.model;
 
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
-    public class Ball extends MovableObject {
-        private final double radius;
-        private final int worldWidth, worldHeight;
+public class Ball extends MovableObject {
+    private final double radius;
 
-        public Ball(double x, double y, double radius, double dx, double dy, int worldWidth, int worldHeight) {
-            super(x, y, radius * 2, radius * 2, dx, dy);
-            this.radius = radius;
-            this.worldWidth = worldWidth;
-            this.worldHeight = worldHeight;
-        }
-        @Override
-        public void update(double dt) {
-            // Cập nhật vị trí dựa theo vận tốc
-            setX(getX() + getDx() * dt);
-            setY(getY() + getDy() * dt);
-        }
-
-        @Override
-        public void render(GraphicsContext gc) {
-
-        }
+    public Ball(double x, double y, double radius, double dx, double dy) {
+        super( x,  y, 0,0, dx, dy);
+        this.radius = radius;
     }
+
+    @Override
+    public void render(GraphicsContext gc) {
+        gc.setFill(Color.WHITE);
+        gc.fillOval(getX(), getY(), getWidth(), getHeight());
+    }
+}
+
