468abf399da26f710c48ee7ba352176240f0fcda
ngochuong123 <24021511@vnu.edu.vn>
2025-10-22 17:03:43 +0700

    feat:update App

--- src/main/java/vn/uet/oop/arkanoid/App.java
+++ src/main/java/vn/uet/oop/arkanoid/App.java
@@ -20,67 +20,6 @@ public class App extends Application {
     public void start(Stage stage) {
         MenuController menuController = new MenuController(stage);
         menuController.eventMenu();
-        // --- Tạo Canvas ---
-        Canvas canvas = new Canvas(GameConfig.SCREEN_WIDTH, GameConfig.SCREEN_HEIGHT);
-        GraphicsContext gc = canvas.getGraphicsContext2D();
-
-        // --- Tạo GameManager ---
-        gameManager = new GameManager();
-
-        // --- Scene & Stage ---
-        StackPane root = new StackPane(canvas);
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.setTitle("Arkanoid - JavaFX Edition");
-        stage.show();
-
-        // --- Xử lý phím ---
-        scene.setOnKeyPressed(event -> {
-            if (event.getCode() == KeyCode.LEFT || event.getCode() == KeyCode.A) {
-                leftPressed = true;
-            } else if (event.getCode() == KeyCode.RIGHT  || event.getCode() == KeyCode.D) {
-                rightPressed = true;
-            } if (event.getCode() == KeyCode.SPACE) {
-                gameManager.launchBall();
-            }
-        });
-
-        scene.setOnKeyReleased(event -> {
-            if (event.getCode() == KeyCode.LEFT   || event.getCode() == KeyCode.A) {
-                leftPressed = false;
-            } else if (event.getCode() == KeyCode.RIGHT    || event.getCode() == KeyCode.D) {
-                rightPressed = false;
-            }
-        });
-
-
-
-
-
-        // --- Game Loop ---
-        AnimationTimer timer = new AnimationTimer() {
-            private long lastTime = 0;
-
-            @Override
-            public void handle(long now) {
-                if (lastTime == 0) {
-                    lastTime = now;
-                    return;
-                }
-
-                double deltaTime = (now - lastTime) / 1e9; // nano -> giây
-                lastTime = now;
-
-                // Cập nhật logic
-                gameManager.update(deltaTime, leftPressed, rightPressed);
-
-                // Vẽ lại màn hình
-                gc.setFill(Color.BLACK);
-                gc.fillRect(0, 0, GameConfig.SCREEN_WIDTH, GameConfig.SCREEN_HEIGHT);
-                gameManager.render(gc);
-            }
-        };
-        timer.start();
     }
 
     public static void main(String[] args) {
